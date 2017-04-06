cbbe6de6735e8ac9bc9715e564add0461d7ba356
janistan <janis_giovani@ymail.com>
2017-04-07 01:41:38 +0700

    library
    library home, collection

---
+++ application/controllers/Library.php
@@ -0,0 +1,133 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Library extends CI_Controller {
+
+    var $template = 'template_library';
+
+    public function home()
+    {
+        $data['title'] = 'Library LMS';
+        $data['topnavigation'] = 'library/library_topnavigation';
+        $data['content'] = 'library/library_home_view';
+        $this->load->view($this->template, $data);
+    }
+
+    public function collection()
+    {
+        $data['title'] = 'Library LMS';
+        $data['topnavigation'] = 'library/library_topnavigation';
+        $data['content'] = 'library/library_collection_view';
+        $this->load->view($this->template, $data);
+    }
+
+    public function about()
+    {
+        $data['title'] = 'Library LMS';
+//        //$data['sidebar'] = 'library/library_sidebar';
+        $data['topnavigation'] = 'library/library_topnavigation';
+        $data['content'] = 'library/library_about_view';
+        $this->load->view($this->template, $data);
+    }
+
+    public function learning_report()
+    {
+        $data['title'] = 'Library LMS';
+        //$data['sidebar'] = 'library/library_sidebar';
+        $data['topnavigation'] = 'library/library_topnavigation';
+        $data['content'] = 'library/learning_report_view';
+        $this->load->view($this->template, $data);
+    }
+
+    public function classScheduleView()
+    {
+        $data['title'] = 'SMS';
+        //$data['sidebar'] = 'library/library_sidebar';
+        $data['topnavigation'] = 'library/library_topnavigation';
+        $data['content'] = 'includes/class_schedule_view';
+        $this->load->view($this->template, $data);
+    }
+
+    public function examScheduleView()
+    {
+        $data['title'] = 'SMS';
+        //$data['sidebar'] = 'library/library_sidebar';
+        $data['topnavigation'] = 'library/library_topnavigation';
+        $data['content'] = 'includes/exam_schedule_view';
+        $this->load->view($this->template, $data);
+    }
+
+    public function courseAssignmentQuiz()
+    {
+        $data['title'] = 'Library LMS';
+        //$data['sidebar'] = 'library/library_sidebar';
+        $data['topnavigation'] = 'library/library_topnavigation';
+        $data['content'] = 'library/library_course_qna_view';
+        $this->load->view($this->template, $data);
+    }
+
+    public function courseMaterial()
+    {
+        $data['title'] = 'Library LMS';
+        //$data['sidebar'] = 'library/library_sidebar';
+        $data['topnavigation'] = 'library/library_topnavigation';
+        $data['content'] = 'library/library_course_material_view';
+        $this->load->view($this->template, $data);
+    }
+
+    public function coursePlan()
+    {
+        $data['title'] = 'Library LMS';
+        //$data['sidebar'] = 'library/library_sidebar';
+        $data['topnavigation'] = 'library/library_topnavigation';
+        $data['content'] = 'library/library_course_plan_view';
+        $this->load->view($this->template, $data);
+    }
+
+    public function courseLibrary()
+    {
+        $data['title'] = 'Library LMS';
+        //$data['sidebar'] = 'library/library_sidebar';
+        $data['topnavigation'] = 'library/library_topnavigation';
+        $data['content'] = 'library/library_course_library_view';
+        $this->load->view($this->template, $data);
+    }
+
+    public function courseView()
+    {
+        $data['title'] = 'Library LMS';
+        //$data['sidebar'] = 'library/library_sidebar';
+        $data['topnavigation'] = 'library/library_topnavigation';
+        $data['content'] = 'library/library_course_view';
+        $this->load->view($this->template, $data);
+    }
+
+    public function courseImplementation()
+    {
+        $data['title'] = 'Library LMS';
+        //$data['sidebar'] = 'library/library_sidebar';
+        $data['topnavigation'] = 'library/library_topnavigation';
+        $data['content'] = 'library/library_course_view';
+        $this->load->view($this->template, $data);
+    }
+
+    public function library_profile()
+    {
+        $data['title'] = 'Library LMS';
+        //$data['sidebar'] = 'library/library_sidebar';
+        $data['topnavigation'] = 'library/library_topnavigation';
+        $data['content'] = 'library/library_profile_view';
+        $this->load->view($this->template, $data);
+    }
+
+
+
+    public function library_profile_edit()
+    {
+        $data['title'] = 'Library LMS';
+        //$data['sidebar'] = 'library/library_sidebar';
+        $data['topnavigation'] = 'library/library_topnavigation';
+        $data['content'] = 'library/library_profile_edit_view';
+        $this->load->view($this->template, $data);
+    }
+}
---
+++ application/views/library/library_topnavigation.php
@@ -0,0 +1,49 @@
+
+<!--<div class="banner" id="home">-->
+    <!-- header -->
+    <header>
+        <div class="container">
+
+            <!-- navigation -->
+            <nav class="navbar navbar-default">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <div class="w3-logo">
+                        <h1><a href="<?php echo base_url() ?>index.php/library/home">Library</a></h1>
+<!--                        <label></label>-->
+                    </div>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="<?php echo base_url() ?>index.php/library/home" class="active" href="index.html">Home</a></li>
+                        <li><a href="<?php echo base_url() ?>index.php/library/collection">Collection</a></li>
+                        <li><a href="<?php echo base_url() ?>index.php/library/about">About</a></li>
+
+                        <li><a href="contact.html">Contact</a></li>
+
+                    </ul>
+
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"><span ></span> Sign In</a></li>
+                    </ul>
+
+
+            </nav>
+            <div class="clearfix"></div>
+            <!-- //navigation -->
+        </div>
+    </header>
+    <!-- //header -->
+    <!-- banner-text -->
+
+<!--</div>-->
+
+
